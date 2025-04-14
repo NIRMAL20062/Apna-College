56ce0ec05db0a32c66379725f8bd35d668305768
NIRMAL20062 <kumar.nirmal2608@gmail.com>
2025-04-14 14:09:50 +0530

    `Refactor: Update PrintingRepeatingNumber.java with optimized code and corrected matrix initialization.`

--- Java Programming Language/Array 2/Assignment/PrintingRepeatingNumber.java
+++ Java Programming Language/Array 2/Assignment/PrintingRepeatingNumber.java
@@ -1,9 +1,9 @@
 public class PrintingRepeatingNumber {
     public static void main(String[] args) {
         int[][] matrix = {  { 10, 20, 30, 40 }, 
-        { 15, 25, 33, 45 },
-        { 20, 29, 37, 48 }, 
-        { 32, 33, 39, 50 } };
+                            { 15, 25, 33, 45 },
+                            { 20, 29, 37, 48 }, 
+                            { 32, 33, 39, 50 }  };
 
         int target = 33;
         printRepeatingNumberCount(matrix, target);
@@ -22,7 +22,7 @@ public class PrintingRepeatingNumber {
                 System.out.println("Number " + target + " is repeating " + count + " times");
         }
 
-        // optimized code 
+        // Optimized - Code
             public static void printRepeatingNumberCountOptimized(int[][] matrix, int target) {
                 int row = 0;
                 int col = matrix[0].length - 1;
@@ -33,15 +33,12 @@ public class PrintingRepeatingNumber {
                         count++;
                         col--; // Still go left to find other possible duplicates in the same row
                     } else if (matrix[row][col] > target) {
-                        col--;
+                        col--;   // move left
                     } else {
-                        row++;
+                        row++;   // MOVE DOWN 
                     }
                 }
-            
                 System.out.println("Number " + target + " is repeating " + count + " times (Optimized)");
-            }
-            
-
+            }  
 }
 
