5bf4877fa475bb564d644f2374a41b0429c93c21
NIRMAL20062 <kumar.nirmal2608@gmail.com>
2025-05-16 05:45:13 +0530

    Refactor Input.java to improve user input handling and enhance prompts; replace next() with nextLine() for full name input.

--- Java Programming Language/Vaiables and Data-Types/Input.java
+++ Java Programming Language/Vaiables and Data-Types/Input.java
@@ -18,6 +18,12 @@ import java.util.*;
 public class Input {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.println("Enter your name: ");
+            String name = sc.next();
+            System.out.println("Hello " + name);
+
+            // String name = sc.nextLine(); // this will take the whole line
             System.out.println("Enter your name: ");
             String input = sc.nextLine();
             System.out.println("Hello, " + input + "!\nHow are you?");
@@ -33,18 +39,17 @@ public class Input {
             int c = number + (int) floatNumber;
             System.out.println("Sum of Both = " + c);
 
+            System.out.println("Enter a double: ");
+            double doubleNumber = sc.nextDouble();
+            System.out.println("You entered: " + doubleNumber);
 
+            System.out.println("Enter a Float: ");
             float rad = sc.nextFloat();
             float area_of_circle = 3.14f * rad* rad; 
             System.out.println(area_of_circle);
-
-
         }
     }
 }
-
-
-
 // Scanner is a class in java.util package which is used to get user input from the keyboard. Scanner Obj. uses different- different function to capture different types of data-types and store them in variables.
 
 // Examples uges
