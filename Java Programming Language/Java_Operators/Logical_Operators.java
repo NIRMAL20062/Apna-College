1c316b3ea9729280e84439413b9188fa703f9443
NIRMAL20062 <kumar.nirmal2608@gmail.com>
2025-05-17 11:49:27 +0530

    Update Logical_Operators.java to correct comments and enhance clarity on logical operations

--- Java Programming Language/Java_Operators/Logical_Operators.java
+++ Java Programming Language/Java_Operators/Logical_Operators.java
@@ -3,9 +3,30 @@ public class Logical_Operators {
 
         // logical AND --  $$
         // logical OR --   ||
-        // logical AND --  !
+        // logical not --  ~
         boolean a = true;
         boolean b = false;
         System.out.println("Logical AND: " + (a && b)); // false
+
+
+        // logical OR
+        System.out.println("Logical OR: " + (a || b)); // true
+        // logical NOT
+        System.out.println("Logical NOT: " + (!a)); // false
+        // logical XOR
+        System.out.println("Logical XOR: " + (a ^ b)); // true
+        // logical NAND
+        System.out.println("Logical NAND: " + (!(a && b))); // true
+        // logical NOR
+        System.out.println("Logical NOR: " + (!(a || b))); // false
+        // logical XNOR
+        System.out.println("Logical XNOR: " + (!(a ^ b))); // false
+        // logical implication
+        System.out.println("Logical Implication: " + (!a || b)); // false
+        // logical equivalence
+        System.out.println("Logical Equivalence: " + (a == b)); // false
+        // logical biconditional
+        System.out.println("Logical Biconditional: " + (a != b)); // true
+
      }
 }
