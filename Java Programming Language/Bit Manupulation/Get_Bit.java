15f6693213eb0d547c3dff4b3d8209d2909f59fb
NIRMAL20062 <kumar.nirmal2608@gmail.com>
2025-05-22 09:26:07 +0530

    Add updateBit and Clear_Last_I_Bit methods to Get_Bit.java for enhanced bit manipulation

--- Java Programming Language/Bit Manupulation/Get_Bit.java
+++ Java Programming Language/Bit Manupulation/Get_Bit.java
@@ -27,6 +27,30 @@ public class Get_Bit {
     public static int clearBit(int num, int pos) {
         return num & ~(1<<pos); // AND with NOT of 1 shifted left by pos
     }
+
+    public static int updateBit(int num, int pos, int bit) {
+        if (bit == 0) {
+            return clearBit(num, pos); // Clear the bit at pos
+        } else {
+            return setBit(num, pos); // Set the bit at pos
+        }
+    }
+
+    public static int updateBit2(String[] args) {
+        // short implementation using bitwise operators
+        int num = Integer.parseInt(args[0]);
+        int pos = Integer.parseInt(args[1]);
+        int bit = Integer.parseInt(args[2]);
+        return (bit == 0)? (num & ~(1 << pos)) : (num | (1 << pos));
+        
+        
+    }
+
+    public static int Clear_Last_I_Bit(int num) {
+        // Clear last i bits of a number
+        int i = 3; // Number of bits to clear
+        return num & (~0 << i); // AND with NOT of 0 shifted left by i
+    }
 }
 // ! Working of above code
 // if num = 5 (binary 101) and pos = 1, then (num & (1<<pos)) = 00101 & 00001 = 00001 = 1
