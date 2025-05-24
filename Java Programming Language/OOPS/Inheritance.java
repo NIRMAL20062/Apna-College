4640b05ae9261427943dfada96fce5ddcf53b098
NIRMAL20062 <kumar.nirmal2608@gmail.com>
2025-05-24 07:15:14 +0530

    Implement Inheritance example with Student and GraduateStudent classes; demonstrate constructor usage and object creation

---
+++ Java Programming Language/OOPS/Inheritance.java
@@ -0,0 +1,24 @@
+public class Inheritance {
+    public static void main(String[] args) {
+        // Creating an object of the Student class
+        Student student1 = new Student("John Doe", 123);
+        System.out.println("Student Name: " + student1.name + ", Roll: " + student1.roll);
+
+        // Creating an object of the GraduateStudent class
+        GraduateStudent gradStudent1 = new GraduateStudent("Jane Smith", 456, "Computer Science");
+        System.out.println("Graduate Student Name: " + gradStudent1.name + ", Roll: " + gradStudent1.roll + ", Major: " + gradStudent1.major);
+    }
+    // make graduate student class
+    static class GraduateStudent extends Student {
+        String major;
+
+        // Constructor for GraduateStudent
+        GraduateStudent(String name, int roll, String major) {
+            super(name, roll); // Call the constructor of the parent class (Student)
+            this.major = major;
+        }
+    }
+}
+
+
+// r : Nhi likhoge toh bhi chalega quki , constructor already directory me hai usko java use krega
